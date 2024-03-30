6231494c01f567bcbe89875b5731bebc328d8539
Gabriel Villarinho <gabrielsoaresvillarinho1@gmail.com>
2024-03-30 15:00:00 -0300

    feat: Galeria de jogos com responsividade implementado

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { GalleriaModule } from 'primeng/galleria';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [GalleriaModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
 
+  isMobileView: boolean = false;
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: unknown) {
+    this.checkScreenWidth();
+  }
+
+  ngOnInit(): void {
+    this.checkScreenWidth();
+  }
+
+
+  checkScreenWidth() {
+    this.isMobileView = window.innerWidth <= 900;
+  }
+
+  images = [
+    { itemImageSrc: 'assets/svg/group/group-1.svg' },
+    { itemImageSrc: 'assets/svg/group/group-2.svg' },
+    { itemImageSrc: 'assets/svg/group/group-3.svg' },
+    { itemImageSrc: 'assets/svg/group/group-4.svg' },
+  ];
 }
